a10b40946905b5e9843d19fc6202ad8b86d69a9b
ChengHua926 <huacheng615@gmail.com>
2023-11-05 16:51:22 -0500

    flutter extension

--- harmone-extension/src/App.js
+++ harmone-extension/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Page 1 Component
+const WelcomePage = () => (
+  <div>
+    <div>Welcome</div>
+    <Link to="/next" style={{ position: 'fixed', bottom: '20px', right: '20px' }}>→</Link>
+  </div>
+);
+
+// Page 2 Component
+const NextPage = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div>Meet Harmone AI and watch YouTube with it</div>
+    <Link to="/session"><button style={{ marginTop: '20px' }}>Start Session</button></Link>
+  </div>
+);
+
+// Session Page Component
+const SessionPage = () => (
+  <div>
+    {/* Your session page content here */}
+  </div>
+);
+
+// App Component with Routing
+const App = () => (
+  <Router>
+    <Route exact path="/" component={WelcomePage} />
+    <Route path="/next" component={NextPage} />
+    <Route path="/session" component={SessionPage} />
+  </Router>
+);
 
 export default App;
