ef33e06d0d828f791ceccdc2588f49538c475748
ChengHua926 <huacheng615@gmail.com>
2023-11-05 20:28:26 -0500

    url fetched

--- harmone_extension_f/lib/screens/intro.dart
+++ harmone_extension_f/lib/screens/intro.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:harmone_extension_f/screens/chat_history.dart';
 import 'package:harmone_extension_f/providers/url.dart';
-import 'package:js/js.dart';
 import 'package:provider/provider.dart';
-import 'dart:js' as js;
-
-
-@JS('chrome.runtime.sendMessage')
-external void sendMessage(Object message, void Function(String) responseCallback);
-
-void requestCurrentTabUrl(BuildContext context) {
-  js.context.callMethod('sendMessageToBackground', [
-    {'message': 'get_url'},
-    (String url) {
-      print('Current tab URL: $url');
-      Provider.of<Url>(context, listen: false).updateURL(url);
-    },
-  ]);
-}
+import 'dart:js_util';
+import 'package:harmone_extension_f/providers/chrome_api.dart' as chrome_api;
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -27,6 +13,24 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
+  Future<void> _getCurrentTabUrl() async {
+    try {
+      List<chrome_api.Tab> tabs = await promiseToFuture(
+        chrome_api.query(chrome_api.ParameterQueryTabs(
+            active: true, lastFocusedWindow: true)),
+      );
+      if (tabs.isNotEmpty && tabs.first.url.isNotEmpty) {
+        Provider.of<Url>(context, listen: false).updateURL(tabs.first.url);
+        print('Current tab URL: ${tabs.first.url}');
+      }
+      else {
+        print('No tabs found');
+      }
+    } catch (e) {
+      print('Failed to get current tab URL: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,15 +49,14 @@ class _IntroPageState extends State<IntroPage> {
               const Text(
                 'Meet Jessica, she is excited to meet you and watch this YouTube video with you',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign:
-                    TextAlign.center, // Centers the text within the Text widget
+                textAlign: TextAlign.center,
               ),
-              const SizedBox(
-                  height: 20), // Adds spacing between the text and button
+              const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  // requestCurrentTabUrl(
-                  //     context); // pass context to requestCurrentTabUrl
+                onPressed: () async {
+                  // Fetch the current tab URL before navigating
+                  await _getCurrentTabUrl();
+                  // Navigate to ChatHistoryPage
                   Navigator.of(context).push(
                     MaterialPageRoute(
                       builder: (context) => const ChatHistoryPage(),
