60e03f64666e1db15165acc09c3cb1f784796fe0
lucychi <luluchi@uw.edu>
2023-11-21 16:54:47 -0500

    update audio recording

--- frontend extension/lib/screens/intro.dart
+++ frontend extension/lib/screens/intro.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
@@ -7,10 +8,8 @@ import 'package:harmone_extension_f/providers/url.dart';
 import 'package:harmone_extension_f/providers/audio_record.dart';
 import 'package:provider/provider.dart';
 import 'dart:js_util' as js_util;
-import 'package:harmone_extension_f/providers/chrome_api.dart' as chrome_api;
 import 'dart:js' as js;
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -20,25 +19,7 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
-  late String ytUrl;
 
-
-  Future<http.Response> createPostRequest(String url, ) async {
-    var data = {
-      "ytUrl": ytUrl,
-      "timestamp": xxx,
-      "audio_files": yyy,
-    };
-    
-    return await http.post(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(data),
-    );
-  }
-  
   void sendMessageToContentScript() async {
     var sendMessage =
         js_util.getProperty(js.context['chrome']['runtime'], 'sendMessage');
@@ -50,41 +31,6 @@ class _IntroPageState extends State<IntroPage> {
         print('Response from content script: $result');
       })
     ]));
-  }
-
-  void getCurrentYoutubeTimestamp() async {
-    print("calling getCurrentYoutubeTimestamp");
-
-    var sendMessage =
-        js_util.getProperty(js.context['chrome']['runtime'], 'sendMessage');
-    var response =
-        await js_util.promiseToFuture(js_util.callMethod(sendMessage, [], [
-      {
-        'action': 'getYoutubeVideoCurrentTime',
-      },
-      js.allowInterop((result) {
-        print('Current video time: $result');
-      })
-    ]));
-  }
-
-  Future<String> _getCurrentTabUrl() async {
-    try {
-      List<chrome_api.Tab> tabs = await js_util.promiseToFuture(
-        chrome_api.query(chrome_api.ParameterQueryTabs(
-            active: true, lastFocusedWindow: true)),
-      );
-      if (tabs.isNotEmpty && tabs.first.url.isNotEmpty) {
-        ytUrl = tabs.first.url;
-        Provider.of<Url>(context, listen: false).updateURL(ytUrl);
-        print('Current tab URL: ${ytUrl}');
-        return ytUrl;
-      } else {
-        throw Exception('No tabs found');
-      }
-    } catch (e) {
-      throw Exception('Failed to get current tab URL: $e');
-    }
   }
 
   @override
@@ -134,7 +80,8 @@ class _IntroPageState extends State<IntroPage> {
                       onPressed: () async {
                         sendMessageToContentScript();
                         String url =
-                            await _getCurrentTabUrl(); // Fetch the current tab URL
+                            // await getCurrentTabUrl(); 
+                            Provider.of<AudioRecord>(context as BuildContext, listen: false).getCurrentTabUrl() as String; // Fetch the current tab URL
                         Navigator.of(context).push(
                           MaterialPageRoute(
                             builder: (context) => const ChatHistoryPage(),
